9f8313422fcce128e2a39dee80da8341e49abfa7
piotrpoleszak <poleszak@protonmail.com>
2023-03-16 23:40:46 +0100

    Prepare RabbitMQ module

--- rabbitmq/src/main/java/com/poleszak/rabbitmq/RabbitMQApplication.java
+++ rabbitmq/src/main/java/com/poleszak/rabbitmq/RabbitMQApplication.java
@@ -1,6 +1,6 @@
-package com.poleszak;
+package com.poleszak.rabbitmq;
 
-public class Main {
+public class RabbitMQApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
