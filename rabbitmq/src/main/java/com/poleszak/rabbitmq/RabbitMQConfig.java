85411a692ce6b3a97f67abc8c45e885480c75a2f
piotrpoleszak <poleszak@protonmail.com>
2023-03-18 17:28:55 +0100

    Create RabbitMQConfig

--- rabbitmq/src/main/java/com/poleszak/rabbitmq/RabbitMQConfig.java
+++ rabbitmq/src/main/java/com/poleszak/rabbitmq/RabbitMQConfig.java
@@ -1,12 +1,9 @@
 package com.poleszak.rabbitmq;
 
 import lombok.AllArgsConstructor;
-import org.postgresql.core.ConnectionFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @AllArgsConstructor
-public class RabbitMQApplication {
-
-    private final ConnectionFactory connectionFactory;
+public class RabbitMQConfig {
 }
