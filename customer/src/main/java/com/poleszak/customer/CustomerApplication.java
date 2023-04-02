a4fb10c4a8fc0c25abd2389c041e815484a9f654
piotrpoleszak <poleszak@protonmail.com>
2023-04-02 17:33:52 +0200

    Configure profiles

--- customer/src/main/java/com/poleszak/customer/CustomerApplication.java
+++ customer/src/main/java/com/poleszak/customer/CustomerApplication.java
@@ -3,6 +3,8 @@ package com.poleszak.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -11,6 +13,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         }
 )
 @EnableFeignClients(basePackages = "com.poleszak.clients")
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
