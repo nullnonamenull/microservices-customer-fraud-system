0e961fe9108c394dacbe14be779d6bbc0467cb51
piotrpoleszak <poleszak@protonmail.com>
2023-04-03 21:22:47 +0200

    Prepare tests customer

---
+++ customer/src/test/java/com/poleszak/customer/unit/service/CustomerServiceTest.java
@@ -0,0 +1,12 @@
+package com.poleszak.customer.unit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @Test
+    void addNewCustomer() {
+    }
+}
