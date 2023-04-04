525f795ad89ef5b9b6d4447a6a3d9d08c718db75
piotrpoleszak <poleszak@protonmail.com>
2023-04-04 22:28:10 +0200

    Prepare tests customer

--- customer/src/test/java/com/poleszak/customer/unit/service/CustomerServiceTest.java
+++ customer/src/test/java/com/poleszak/customer/unit/service/CustomerServiceTest.java
@@ -1,12 +1,33 @@
 package com.poleszak.customer.unit.service;
 
+import com.poleszak.clients.fraud.FraudClient;
+import com.poleszak.customer.repository.CustomerRepository;
+import com.poleszak.customer.service.CustomerService;
+import com.poleszak.rabbitmq.RabbitMQMessageProducer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
+
+    @Mock
+    private CustomerRepository customerRepository;
+    @Mock
+    private FraudClient fraudClient;
+    @Mock
+    private RabbitMQMessageProducer messageProducer;
+    @InjectMocks
+    private CustomerService customerService;
 
     @Test
     void addNewCustomer() {
+        //given
+        //when
+        //then
     }
 }
